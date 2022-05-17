028cb92332240e8a36fe358903caffd3bac581a1
Derek Kropff <70429729+MrWolpertinger@users.noreply.github.com>
2022-05-17 15:22:55 +0200

    noi(nothing of importance)

--- Multi-Host-Services/Multi-Host-Services/Service1.cs
+++ Multi-Host-Services/Multi-Host-Services/Service1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System;//
+using System.Collections.Generic;//
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Linq;
+using System.Linq;//
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text;//
+using System.Threading.Tasks;//
 
 namespace Multi_Host_Services
 {
