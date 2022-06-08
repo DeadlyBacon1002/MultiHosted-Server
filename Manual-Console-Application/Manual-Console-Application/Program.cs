bfa11389e579c175967f5bab2da64bfedb3e5026
Derek Kropff <70429729+MrWolpertinger@users.noreply.github.com>
2022-06-08 21:31:23 +0200

    Structure
    Added more structural logic without functionality

--- Manual-Console-Application/Manual-Console-Application/Program.cs
+++ Manual-Console-Application/Manual-Console-Application/Program.cs
@@ -24,29 +24,54 @@ namespace Multi_Host_Services_Manual
             {
                 var autoEvent = new AutoResetEvent(false);
                 var aTimer = new System.Threading.Timer(OnTimedEvent, autoEvent, (60 * 60 * 1000), (60 * 60 * 1000));// start backup cycle every 60 minutes
-
-
                 Console.WriteLine("press enter to exit");
                 Console.ReadLine();
+                aTimer.Dispose();
+                EndCycle();
                 //exit cycle
             }
         }
 
-        private static int StartCycle()// run to determind ehosting and ititial actions if those actions can be taken
+        private static int StartCycle()// run to determind hosting and initial actions if those actions can be taken
         {
             //check serverstatus
+            if(!checkServerStatus())
+            {
+                //check allocation
+                if (!checkDNSFlag())
+                {
+                    //set allocation
+                    setDNSFlag(true);
+                    updateDNS();
+                    //run download serverfiles
+                    fileDownload();
+                    interGameBK(true);
+                    //Start Server |||||||||||||||||||||||||||||||||||||||||||||
+                    return 0;
+                } else
+                {
+                    return 2;
+                }
+            } else
+            {
+                return 1;
+            }
+        }
 
-            //check allocation
-
-            //run first cycle
+        private static int EndCycle()
+        {
+            //deallocate recourses
+            setDNSFlag(false);
+            //stop server ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+            //run end of session cyle
+            interGameBK(false);
             return 0;
         }
 
-        private static void OnTimedEvent(object source)
+        private static void OnTimedEvent(object source)//backup cycle
         {
-            Console.WriteLine(source.ToString());
+            fileBK();
         }
-
 
         private static void updateDNS()
         {
@@ -60,14 +85,16 @@ namespace Multi_Host_Services_Manual
             //rename "true.MD" to "false.MD" or the otherway around
         }
 
-        private static void checkServerStatus()
+        private static Boolean checkServerStatus()//returns true if server is runnung
         {
             //https://mcapi.us/server/status?ip=s.nerd.nu&port=25565
+            return true;
         }
 
-        private static void checkDNSFlag()
+        private static Boolean checkDNSFlag()//returns true if file "true.MD" exists
         {
             //check if file "true.MD" exists
+            return true;
         }
 
         private static void fileBK()
@@ -92,96 +119,6 @@ namespace Multi_Host_Services_Manual
 
     public class API_Call
     {
-        static HttpClient client = new HttpClient();
-
-        static async Task<Uri> CreateProductAsync(Product product)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync(
-                "api/products", product);
-            response.EnsureSuccessStatusCode();
-
-            // return URI of the created resource.
-            return response.Headers.Location;
-        }
-
-        static async Task<Boolean> GetProductAsync(string path)
-        {
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        static async Task<Product> UpdateProductAsync(Product product)
-        {
-            HttpResponseMessage response = await client.PutAsJsonAsync(
-                $"api/products/{product.Id}", product);
-            response.EnsureSuccessStatusCode();
-
-            // Deserialize the updated product from the response body.
-            product = await response.Content.ReadAsAsync<Product>();
-            return product;
-        }
-
-        static async Task<HttpStatusCode> DeleteProductAsync(string id)
-        {
-            HttpResponseMessage response = await client.DeleteAsync(
-                $"api/products/{id}");
-            return response.StatusCode;
-        }
-
-        static void Main()
-        {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync()
-        {
-            // Update port # in the following line.
-            client.BaseAddress = new Uri("http://localhost:64195/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            try
-            {
-                // Create a new product
-                Product product = new Product
-                {
-                    Name = "Gizmo",
-                    Price = 100,
-                    Category = "Widgets"
-                };
-
-                var url = await CreateProductAsync(product);
-                Console.WriteLine($"Created at {url}");
-
-                // Get the product
-                product = await GetProductAsync(url.PathAndQuery);
-                ShowProduct(product);
-
-                // Update the product
-                Console.WriteLine("Updating price...");
-                product.Price = 80;
-                await UpdateProductAsync(product);
-
-                // Get the updated product
-                product = await GetProductAsync(url.PathAndQuery);
-                ShowProduct(product);
-
-                // Delete the product
-                var statusCode = await DeleteProductAsync(product.Id);
-                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.ReadLine();
-        }
+        
     }
 }
