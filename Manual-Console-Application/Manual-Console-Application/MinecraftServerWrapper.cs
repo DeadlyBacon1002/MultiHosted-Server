6f6ae5fb368a93c9e78a436a750c614aaf0f8807
Derek Kropff <70429729+MrWolpertinger@users.noreply.github.com>
2022-06-16 00:02:13 +0200

    console output
    Redirected server output to program terminal

--- Manual-Console-Application/Manual-Console-Application/MinecraftServerWrapper.cs
+++ Manual-Console-Application/Manual-Console-Application/MinecraftServerWrapper.cs
@@ -4,17 +4,20 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 namespace Manual_Console_Application
 {
     public class MinecraftServerWrapper
     {
-        static Process process = null;
+        public Process process = null;
+        private StringBuilder output = new StringBuilder();
         public MinecraftServerWrapper(string[] args)
         { //java -Xmx2G -jar fabric-server-mc.1.18.2-loader.0.14.7-launcher.0.11.0.jar nogui
             ProcessStartInfo processToRunInfo = new ProcessStartInfo("java", "-Xmx" + args[0] + "G -jar fabric-server-mc.1.18.2-loader.0.14.7-launcher.0.11.0.jar nogui");
             processToRunInfo.RedirectStandardInput = true;
+            processToRunInfo.RedirectStandardOutput = true;
             processToRunInfo.CreateNoWindow = true;
             processToRunInfo.UseShellExecute = false;
             processToRunInfo.WorkingDirectory = @"" + args[1];
@@ -22,17 +25,47 @@ namespace Manual_Console_Application
             process = new Process();
             process.StartInfo = processToRunInfo;
             //process.Close();
+            /*process.Exited += new DataReceivedEventHandler((sender, e) =>
+            {
+                if (!String.IsNullOrEmpty(e.ToString()))
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            });*/
+
+            
         }
         public async Task Start()
         {
-            Console.WriteLine("Server is starting...");
+            //Console.WriteLine("Server is starting...");//
             process.Start();
+            process.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler((sender, e) =>
+            {
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    output.Clear();
+                    output.Append(e.Data);
+                    Console.WriteLine(output);
+                }
+            });
+            process.BeginOutputReadLine();
         }
 
         public async Task Stop()
         {
-            try { process.StandardInput.WriteLine("stop"); }
+            try { process.StandardInput.WriteLine("stop");
+                process.WaitForExit();
+                process.Close();
+            }
             catch { }
+        }
+
+        public void WriteToServer(string s)
+        {
+            if (!String.IsNullOrEmpty(s))
+            {
+                process.StandardInput.WriteLine(s);
+            }
         }
     }
 }
