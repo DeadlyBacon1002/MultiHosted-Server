5cac7a2feb36f38deac1b844c1de624fc22b452a
Derek Kropff <70429729+MrWolpertinger@users.noreply.github.com>
2022-06-18 11:35:59 +0200

    fixed api issue
    request header user-agent wasn't set

--- Manual-Console-Application/Manual-Console-Application/MinecraftServerWrapper.cs
+++ Manual-Console-Application/Manual-Console-Application/MinecraftServerWrapper.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 
 namespace Manual_Console_Application
